90cc237086cb4c3c970c7df9fe81d32f2329e9e2
Krista Moroder <kmoroder@gmail.com>
2018-10-14 21:05:17 +0200

    adds instructions

--- src/binary_tree.js
+++ src/binary_tree.js
@@ -10,66 +10,20 @@ class BinaryTree {
     this.right = null
   }
 
-  add(value) {
-    if (!this.value) {
-      this.value = new BinaryTree(value);
-    }
-    if (value <= this.value) {
-      if (!this.left) {
-        this.left = new BinaryTree(value);
-      } else {
-        this.left.add(value);
-      }
-    } else {
-      if (!this.right) {
-        this.right = new BinaryTree(value);
-      } else {
-        this.right.add(value);
-      }
-    }
-  }
+  // adds node to left if value is less than or equal to this.value
+  // adds node to right if value is greater than node
+  add(value) {}
 
   // returns true or false if the value is in the tree
-  contains(value) {
-    if (this.value === value) {
-      return true;
-    } else {
-      if (this.right) {
-        return this.right.contains(value);
-      }
-      if (this.left) {
-        return this.left.contains(value);
-      }
-    }
-    return false;
-  }
+  contains(value) {}
 
   // apply callback in this order: left node, current node, right node
-  traverseDepthFirstInOrder(fn) {
-    if (this.left) {
-      this.left.traverseDepthFirstInOrder(fn);
-    }
-    fn(this);
-    if (this.right) {
-      this.right.traverseDepthFirstInOrder(fn);
-    }
+  traverseDepthFirstInOrder(fn) {}
   }
 
   // apply callback from left to right across each level
   // hint: use a queue for this!
-  traverseBreadthFirst(fn) {
-    const queue = [this];
-    while (queue.length) {
-      const node = queue.shift();
-      fn(node);
-      if (node.left) {
-        queue.push(node.left);
-      }
-      if (node.right) {
-        queue.push(node.right);
-      }
-    }
-  }  
+  traverseBreadthFirst(fn) {}  
 
 }
 
